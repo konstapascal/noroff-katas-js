c2f422e11f701aa8c0328efacf884c0a4b1ed474
konstapascal <pascalkonstantinos@gmail.com>
2022-02-18 10:47:55 +0100

    CHORE: Small changes

--- 006-elastic-word/index.ts
+++ 006-elastic-word/index.ts
@@ -3,38 +3,38 @@ export function elasticize(word) {
 
 	word = word.toUpperCase();
 
-	let left = '';
+	let leftSide = '';
 	let center = '';
-	let right = '';
+	let rightSide = '';
 
 	// Word length is odd
 	if (word.length % 2 !== 0) {
 		let middleIdx = Math.ceil(word.length / 2);
 
-		left = word.substring(0, middleIdx - 1);
+		leftSide = word.substring(0, middleIdx - 1);
 		center = word[middleIdx - 1];
-		right = word.substring(middleIdx, word.length);
+		rightSide = word.substring(middleIdx, word.length);
 	}
 
 	// Word length is even
 	if (word.length % 2 === 0) {
 		let halfLength = word.length / 2;
 
-		left = word.substring(0, halfLength);
-		right = word.substring(halfLength, word.length);
+		leftSide = word.substring(0, halfLength);
+		rightSide = word.substring(halfLength, word.length);
 	}
 
 	let finalWord = '';
 
-	// Adding left, center, right
-	for (let i = 0; i < left.length; i++) {
-		finalWord += left[i].repeat(i + 1);
+	// Adding left side, center, right side
+	for (let i = 0; i < leftSide.length; i++) {
+		finalWord += leftSide[i].repeat(i + 1);
 	}
 
-	finalWord += center.repeat(left.length + 1);
+	finalWord += center.repeat(leftSide.length + 1);
 
-	for (let i = 0; i < right.length; i++) {
-		finalWord += right[i].repeat(right.length - i);
+	for (let i = 0; i < rightSide.length; i++) {
+		finalWord += rightSide[i].repeat(rightSide.length - i);
 	}
 
 	return finalWord;
