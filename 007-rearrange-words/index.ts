cbdb252e015571c55ab35d1e3e1b3f45c8183ec0
konstapascal <pascalkonstantinos@gmail.com>
2022-02-21 18:37:33 +0100

    CHORE: Added alternative syntax

--- 007-rearrange-words/index.ts
+++ 007-rearrange-words/index.ts
@@ -1,21 +1,18 @@
 export function rearrange(sentence) {
 	if (sentence.trim() === '') return '';
 
-	const splitSentence = sentence.split(' ');
-	// [{number: 1, word: "This"}, {number: 4, word: "Test"}, ...]
-	const dictionary = splitSentence.map(word => {
-		for (const char of word) {
-			if (isNaN(char)) continue;
-			return {
-				number: Number.parseInt(char),
-				word: word.replace(char, '')
-			};
-		}
-	});
-
-	const sortedDictionary = dictionary.sort((first, second) => first.number - second.number);
-	const words = sortedDictionary.map(obj => obj.word);
-	const finalWord = words.join(' ');
-
-	return finalWord;
+	return sentence
+		.split(' ')
+		.map(word => {
+			for (const char of word) {
+				if (isNaN(char)) continue;
+				return {
+					number: Number.parseInt(char),
+					word: word.replace(char, '')
+				};
+			}
+		})
+		.sort((first, second) => first.number - second.number)
+		.map(obj => obj.word)
+		.join(' ');
 }
